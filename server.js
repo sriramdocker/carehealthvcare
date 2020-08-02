9a3fe7a1b7073bce7b55bb30eb81e3685ffcfb7c
sriramdocker <59905255+sriramdocker@users.noreply.github.com>
2020-08-02 15:41:38 -0700

    commit

--- server.js
+++ server.js
@@ -17,8 +17,13 @@ const ROOM_NAME = "telemedicineAppointment";
 // Max. period that a Participant is allowed to be in a Room (currently 14400 seconds or 4 hours)
 const MAX_ALLOWED_SESSION_DURATION = 14400;
 
-const patientPath = path.join(__dirname, './public/patient.html');
-app.use("/patient", express.static(patientPath));
+//const patientPath = path.join(__dirname, './public/patient.html');
+//app.use("/patient", express.static(patientPath));
+
+app.get('/patient', function (req, res) {
+  res.sendFile(__dirname + './public/patient.html');
+});
+
 
 const providerPath = path.join(__dirname, "./public/provider.html");
 app.use("/provider", express.static(providerPath));
